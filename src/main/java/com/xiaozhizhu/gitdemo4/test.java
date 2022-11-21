29c52eb863f4f40eee8ce58a5b41062936906d5f
sunzhikai <sunzhikai2001@163.com>
2022-11-21 16:50:47 +0800

    commit

---
+++ src/main/java/com/xiaozhizhu/gitdemo4/test.java
@@ -0,0 +1,12 @@
+package com.xiaozhizhu.gitdemo4;
+
+/**
+ * @author Sunzhikai
+ * @version 1.0
+ * @date 2022/11/21 16:47
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("asdada");
+    }
+}
---
+++ src/test/java/com/xiaozhizhu/gitdemo4/GitDemo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaozhizhu.gitdemo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemo4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
